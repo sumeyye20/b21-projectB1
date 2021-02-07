893d743678a42c7bb27f3d6a753ad57804ca0fd4
sumeyye20 <71411837+sumeyye20@users.noreply.github.com>
2021-02-07 22:51:37 +0100

    you are dadicated

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println( " hallo last main method");
         // i send a good messsage to you
+        // you are dedicated
     }
 }
